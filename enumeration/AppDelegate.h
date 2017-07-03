995bc0535ecf8de92c44621a82599cc7434c5cab
macserver <macserver@macservers-mac-pro.local>
2017-07-03 11:33:29 +0530

    Initial Commit

---
+++ enumeration/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  enumeration
+//
+//  Created by macserver on 7/3/17.
+//  Copyright © 2017 macserver. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ enumeration/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  enumeration
+//
+//  Created by macserver on 7/3/17.
+//  Copyright © 2017 macserver. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
